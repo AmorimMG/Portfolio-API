2d01d72e8eebd0ef9b8f4a60a99ddfea495d9bd9
Gabriel Amorim <gabriel._amorim@hotmail.com>
2024-10-01 22:05:57 -0300

    trocando banco de dados para mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,58 +1,58 @@
-import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.dev';
+import { HttpModule } from "@nestjs/axios";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { typeOrmConfig } from "./config/typeorm";
 
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { UsuarioModule } from './usuarios/usuario.module';
-import { ProjetoModule } from './projetos/projetos.module';
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { ProjetoModule } from "./projetos/projetos.module";
+import { UsuarioModule } from "./usuarios/usuario.module";
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MailController } from './mail/mail.controller';
-import { MailService } from './mail/mail.service';
-import { SpotifyController } from './spotify/spotify.controller';
-import { SpotifyService } from './spotify/spotify.service';
-import { DiscordController } from './discord/discord.controller';
-import { DiscordService } from './discord/discord.service';
-import { LastFMController } from './lastFM/lastfm.controller';
-import { LastFMService } from './lastFM/lastfm.service';
-import { SteamController } from './steam/steam.controller';
-import { SteamService } from './steam/steam.service';
-import { AnilistService } from './anilist/anilist.service';
-import { AnilistController } from './anilist/anilist.controller';
-import { GithubService } from './github/github.service';
-import { GithubController } from './github/github.controller';
+import { AnilistController } from "./anilist/anilist.controller";
+import { AnilistService } from "./anilist/anilist.service";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { DiscordController } from "./discord/discord.controller";
+import { DiscordService } from "./discord/discord.service";
+import { GithubController } from "./github/github.controller";
+import { GithubService } from "./github/github.service";
+import { LastFMController } from "./lastFM/lastfm.controller";
+import { LastFMService } from "./lastFM/lastfm.service";
+import { MailController } from "./mail/mail.controller";
+import { MailService } from "./mail/mail.service";
+import { SpotifyController } from "./spotify/spotify.controller";
+import { SpotifyService } from "./spotify/spotify.service";
+import { SteamController } from "./steam/steam.controller";
+import { SteamService } from "./steam/steam.service";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    HttpModule,
-    TypeOrmModule.forRoot(typeOrmConfig),
-    AuthModule,
-    UsuarioModule,
-    ProjetoModule,
-  ],
-  controllers: [
-    AppController,
-    MailController,
-    SpotifyController,
-    DiscordController,
-    LastFMController,
-    SteamController,
-    AnilistController,
-    GithubController,
-  ],
-  providers: [
-    AppService,
-    MailService,
-    SpotifyService,
-    DiscordService,
-    LastFMService,
-    SteamService,
-    AnilistService,
-    GithubService,
-  ],
+	imports: [
+		ConfigModule.forRoot(),
+		HttpModule,
+		TypeOrmModule.forRoot(typeOrmConfig),
+		AuthModule,
+		UsuarioModule,
+		ProjetoModule,
+	],
+	controllers: [
+		AppController,
+		MailController,
+		SpotifyController,
+		DiscordController,
+		LastFMController,
+		SteamController,
+		AnilistController,
+		GithubController,
+	],
+	providers: [
+		AppService,
+		MailService,
+		SpotifyService,
+		DiscordService,
+		LastFMService,
+		SteamService,
+		AnilistService,
+		GithubService,
+	],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,31 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UsuarioModule } from '../usuarios/usuario.module';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy';
-import { LocalStrategy } from './local.strategy';
-import * as dotenv from 'dotenv';
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import * as dotenv from "dotenv";
+import { UsuarioModule } from "../usuarios/usuario.module";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt.strategy";
+import { LocalStrategy } from "./local.strategy";
 
-dotenv.config()
+dotenv.config();
 
 @Module({
-  imports: [
-    UsuarioModule,
-    PassportModule,
-    JwtModule.register({
-      secret: process.env.SECRET,
-      signOptions: { expiresIn: '60m' },
-    }),
-  ],
-  providers: [
-    AuthService,
-    JwtStrategy, 
-    {
-    provide: 'APP_GUARD',
-    useClass: LocalStrategy,
-  }],
-  controllers: [AuthController],
+	imports: [
+		UsuarioModule,
+		PassportModule,
+		JwtModule.register({
+			secret: process.env.JWT_SECRET,
+			signOptions: { expiresIn: "60m" },
+		}),
+	],
+	providers: [
+		AuthService,
+		JwtStrategy,
+		{
+			provide: "APP_GUARD",
+			useClass: LocalStrategy,
+		},
+	],
+	controllers: [AuthController],
 })
 export class AuthModule {}
