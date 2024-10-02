caea03ac3289f7b347d2981964e09dd427909d8f
Gabriel Amorim <gabriel._amorim@hotmail.com>
2024-10-02 04:00:07 -0300

    adicionando ssl

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -9,7 +9,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
 	url: process.env.MONGODB_URI,
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
-	ssl: false,
 	entities: [__dirname + "/../**/*.entity{.ts,.js}"],
 	//synchronize: true, // Em produção, é melhor desabilitar isso
 	synchronize: true, // Em produção, é melhor desabilitar isso
